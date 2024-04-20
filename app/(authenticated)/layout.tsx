1771fa79c90f7fe8ec3f6fd95022f61ebacb344c
Elfennani Nizar <elfennani2002@gmail.com>
2024-04-20 17:53:58 +0100

    fix

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  if (!cookies().has("token")) return redirect("/");
+  if (!cookies().has("token")) return redirect("/auth");
 
   return children;
 }
