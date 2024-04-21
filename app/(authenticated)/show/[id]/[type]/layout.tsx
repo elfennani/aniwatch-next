bb5576d8c77003d8dc834a8b686491dbbecb5158
Elfennani Nizar <elfennani2002@gmail.com>
2024-04-21 13:22:12 +0100

    Fixed skeleton loading for watch page

--- app/(authenticated)/show/[id]/[type]/layout.tsx
+++ app/(authenticated)/show/[id]/[type]/layout.tsx
@@ -3,19 +3,27 @@ import ShowEpisodes from "../components/ShowEpisodes";
 
 export default function Layout({
   children,
-  params: { id, ep },
+  params: { id },
 }: {
   children: React.ReactNode;
-  params: { id: string; ep: string };
+  params: { id: string };
 }) {
-  // const heads = useParams
-
   return (
     <section className="h-dvh flex flex-col">
       <div>{children}</div>
       <div className="p-8 overflow-y-scroll h-full">
-        <ShowEpisodes id={Number(id)} />
+        <Suspense fallback={<ShowEpisodesSkeleton />}>
+          <ShowEpisodes id={Number(id)} />
+        </Suspense>
       </div>
     </section>
   );
 }
+
+const ShowEpisodesSkeleton = () => (
+  <div className="flex flex-col gap-4">
+    <div className="w-full h-[69.75px] bg-zinc-800 rounded-md" />
+    <div className="w-full h-[69.75px] bg-zinc-800 rounded-md" />
+    <div className="w-full h-[69.75px] bg-zinc-800 rounded-md" />
+  </div>
+);
